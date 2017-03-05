dae5db22666441e236c9cc8f57b767d820b3a61b
Arvin-He <510205617@qq.com>
2017-03-05 21:29:03 +0800

    database

--- app/__init__.py
+++ app/__init__.py
@@ -1,8 +1,11 @@
 from flask import Flask
+from flask_mongoengine import MongoEngine
 
 #实例化flask应用
 app = Flask(__name__)
 # 从config加载配置
 app.config.from_object("config")
+
+db = MongoEngine(app)
 
 from app import views, models
--- manage.py
+++ manage.py
@@ -2,11 +2,17 @@
 
 from flask_script import Manager, Server
 from app import app
-
+from app import models
 manager = Manager(app)
 
 
 manager.add_command("runserver", Server(host="127.0.0.1", port=5000, use_debugger=True))
+
+@manager.command
+def save_todo():
+    todo = models.Todo(content="my first todo")
+    todo.save()
+
 
 if __name__ == "__main__":
     manager.run()
