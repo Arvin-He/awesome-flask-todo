82ab57ce50ca0d504f1168e2d0d85204979f1149
Arvin-He <510205617@qq.com>
2017-03-12 15:13:50 +0800

    v1.0

--- app/views.py
+++ app/views.py
@@ -40,3 +40,7 @@ def delete(todo_id):
     todo.delete()
     todos = Todo.objects.all()
     return render_template("index.html", todos=todos)
+
+# @app.errorhandler(404)
+# def not_found(e):
+#     return render_template("404.html"), 404
